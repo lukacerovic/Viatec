19c57c847680e2fa9b6e0f905145c00c60f31c3f
Luka <luka.cerovic14@gmail.com>
2024-12-07 15:21:35 +0100

    Adding contact logic

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,13 +1,17 @@
-import React, { forwardRef, useState } from 'react';
+import React, { forwardRef, useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 
 const Contact = forwardRef((props, ref) => {
   const [formData, setFormData] = useState({
     username: '',
+    lastName: '',
     email: '',
     dateOfBirth: '',
     phone: '',
     state: '',
   });
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -26,8 +30,8 @@ const Contact = forwardRef((props, ref) => {
       });
       const result = await response.json();
       if (response.ok) {
-        alert('Form submitted successfully');
-        setFormData({ username: '', email: '', dateOfBirth: '', phone: '', state: '' });
+        setIsModalOpen(true);
+        setFormData({ username: '', lastName: '', email: '', dateOfBirth: '', phone: '', state: '' });
       } else {
         alert(`Error: ${result.message}`);
       }
@@ -36,9 +40,46 @@ const Contact = forwardRef((props, ref) => {
     }
   };
 
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  useEffect(() => {
+    if (isModalOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+    return () => {
+      document.body.style.overflow = '';
+    };
+  }, [isModalOpen]);
+
+  const Modal = () => {
+    return ReactDOM.createPortal(
+      <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-80 z-50">
+        <div className="bg-white rounded-lg p-6 shadow-lg w-3/4 md:w-1/2 lg:w-1/3">
+          <h2 className="text-xl font-bold mb-4">Vaša forma je uspešno poslata!</h2>
+          <p className="text-base">
+            Hvala Vam na ostavljenim informacijama. Očekujte naš odgovor u najskorijem roku.
+            <br />
+            Srdačan pozdrav, Vaš <span className="font-bold">Viatec</span>.
+          </p>
+          <button
+            onClick={closeModal}
+            className="mt-4 px-4 py-2 bg-[#6c2636] text-white rounded-md hover:bg-[#b72f4f]"
+          >
+            Zatvori
+          </button>
+        </div>
+      </div>,
+      document.getElementById('modal')
+    );
+  };
+
   return (
-    <div className="real-estate-section mt-[10vh] p-5 mx-5" ref={ref}>
-      <form onSubmit={handleSubmit} className="search-form p-3">
+    <div className="real-estate-section mt-[10vh] p-5 mx-5 flex justify-center" ref={ref}>
+      <form onSubmit={handleSubmit} className="search-form p-3 flex flex-col md:justify-between">
         <h3 className="text-2xl px-5">Kontaktirajte nas</h3>
         <p className="text-lg">Popunite formu i očekujte odgovor sa naše strane u najskorijem roku</p>
 
@@ -58,9 +99,9 @@ const Contact = forwardRef((props, ref) => {
             <label>Prezime</label>
             <input
               type="text"
-              name="state"
+              name="lastName"
               placeholder="Prezime"
-              value={formData.state}
+              value={formData.lastName}
               onChange={handleChange}
               required
             />
@@ -118,11 +159,11 @@ const Contact = forwardRef((props, ref) => {
           Pošalji Poruku
         </button>
       </form>
+
+      {isModalOpen && <Modal />}
     </div>
   );
 });
 
 export default Contact;
-
-
 
